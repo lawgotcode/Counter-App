9723bc5e9ddaa414bae46e503b8129e908fe1233
Lawrence Agyei <lawgotcode1990@gmail.com>
2018-08-16 20:46:52 -0500

    styles

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,18 +2,17 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
 	state = {
-		count: 0
+		count: 1
 	};
 
-	styles = {
-        fontSize: 90,
-        fontWeight: "bold"
-	};
+	
 		
 	render() {
+		let classes = "badge m-2 badge-";
+		classes += this.state.count === 0 ? "warning" : "primary";
 		return (
 		<div>
-		<span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+		<span  className={classes}>{this.formatCount()}</span>
 		<button className="btn btn-secondary btn-sm">Increment</button>
 		</div>
      );
